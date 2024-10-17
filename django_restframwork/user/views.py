3ea0133d271ea963046e09893b7a32af3eab6175
TuanTaiTu <phambaanhtuan2003@gmail.com>
2024-10-17 16:49:18 +0700

    done Oauth2, set permission

--- django_restframwork/user/views.py
+++ django_restframwork/user/views.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth import login, logout, authenticate
+from django.http import HttpRequest
+# from django.contrib.auth.models import User
 
 from rest_framework import status, viewsets, serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from oauth2_provider.views import TokenView
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from oauth2_provider.views import TokenView
+from oauth2_provider.contrib.rest_framework.permissions import OAuth2Authentication, TokenHasScope
 
-from .models import Customer, User
+from .models import User
 from .serializers import UserSerializer
 
+import json
+
 class UserViewSet(viewsets.ModelViewSet):
+   authentication_classes = [OAuth2Authentication]
+   permission_classes = [IsAuthenticated]
+   # required_scopes = ['read', 'write']
+   
    queryset = User.objects.all()
    serializer_class = UserSerializer
-   permission_classes = [IsAuthenticated]
 
    @action(methods=['POST'], detail=False, url_path='signup', permission_classes=[AllowAny])
    def signUp(self, request):
       serializer = UserSerializer(data=request.data)
 
       if serializer.is_valid():
-         user = serializer.save()
+         serializer.save()
          print(f'Signed up: {request.data.get("username")}')
          return Response({"message": "Signup successfully!"}, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -34,15 +42,25 @@ class UserViewSet(viewsets.ModelViewSet):
          user = authenticate(request, username=username, password=password)
          
          if user is not None:
-            token_request_data = {
+            login(request, user)
+            
+            token_request = HttpRequest()
+            token_request.method = 'POST'
+            token_request.POST = {
                'grant_type': 'password',
                'username': username,
                'password': password,
                'client_id': 'mXFmoA3VwA5JsZxmFjzNZWD4L6aK8cUHoy090eJl',
-               'client_secret': 'oauth2'
+               'client_secret': 'oauth2',
             }
-            response = TokenView(data=token_request_data)
-            return Response({"message": "Sign in successfully!", "token": response.data}, status=status.HTTP_200_OK)
+            
+            token_view = TokenView.as_view()
+            response = token_view(token_request)
+            response_content = json.loads(response.content.decode('utf-8'))
+            if response.status_code == 200:
+               return Response({"message": "Sign in successfully!", "access_token": response_content["access_token"]}, status=status.HTTP_200_OK)
+            else:
+               return Response({"error": "Error generating token"}, status=response.status_code)
          else:
                return Response({"error": "Invalid credentials!"}, status=status.HTTP_401_UNAUTHORIZED)
       else:
